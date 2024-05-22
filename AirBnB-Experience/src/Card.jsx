0411dd06f47bb8239842d5e78dc41c7b42f6ea57
Diesos <ozturk@live.Fr>
2024-05-22 23:57:37 +0200

    feat: Update Card component to dynamically render image and details

--- AirBnB-Experience/src/Card.jsx
+++ AirBnB-Experience/src/Card.jsx
@@ -1,18 +1,18 @@
 import Katie from './assets/katie-zaferes.png'
 import stars from './assets/star.png'
 
-export default function Card(){
+export default function Card(prop){
 	return(
 		<div className="main-card-container">
 		<div className="card-container">
 			<div className="photo">
-					<img src={Katie} alt="pic of Katie" />
+					<img src={`./assets/ + ${prop.img}`} alt="pic of Katie" />
 			</div>
 			<div className="detail">
 					<span className="standard"><img src={stars} alt="star" />
-					5.0<span className="gray"> (6) • USA</span></span>
-					<p>Life lessons with Katie Zaferes</p>
-					<p><strong>From $136 </strong>/ person</p>
+					{prop.rating}<span className="gray">` ${prop.ratingCount} • ${prop.country}</span></span>
+					<p>{prop.title}</p>
+					<p><strong>`From ${prop.price} `</strong>/ person</p>
 
 			</div>
 		</div>
