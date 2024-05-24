a91e54f2f7c76fcc3f75a91e429882508b3a1404
Diesos <ozturk@live.Fr>
2024-05-24 23:06:41 +0200

    chore: Update Card component to add url-loader dependency and fix image path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import stars from '../assets/star.png'
 
 export default function Card(prop){
-	const origin = window.location.origin
+	// const origin = window.location.origin
 	const cardId = prop.id
 	let badgeText
 	if (prop.openSpots === 0)
@@ -12,7 +12,7 @@ export default function Card(prop){
 		<div className="card-container" key={cardId}>
 			{badgeText &&<div className="card--badge"><p>{badgeText}</p></div>}
 			<div className="photo">
-			<img src={`${origin}/images/${prop.img}`} alt="card Image" />
+			<img src={`../images/${prop.img}`} alt="card Image" />
 
 			</div>
 			<div className="detail">
